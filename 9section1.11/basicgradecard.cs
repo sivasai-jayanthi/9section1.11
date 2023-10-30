9fef5db97fc6acdec91367a4fbddd4543dc5985c
sivasai-jayanthi <sai89938@gmail.com>
2023-10-30 12:13:41 +0530

    Add project files.

---
+++ 9section1.11/basicgradecard.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace _9section1._11
+{
+	public class BasicGradeCard
+	{
+		protected int totalMarks;
+
+		public BasicGradeCard()
+		{
+			totalMarks = 0;
+		}
+
+		public virtual int getTotalMarks()
+		{
+			return totalMarks;
+		}
+	}
+}
---
+++ 9section1.11/elementaryschoolgradecard.cs
@@ -0,0 +1,42 @@
+﻿
+
+namespace _9section1._11
+{
+	public class ElementarySchoolGradeCard : BasicGradeCard
+	{
+		private int maths = 0;
+		private int english = 0;
+		private int secondLanguage = 0;
+		private int socialScience = 0;
+
+		public int Maths
+		{
+			get { return maths; }
+			set { maths = value; }
+		}
+
+		public int English
+		{
+			get { return english; }
+			set { english = value; }
+		}
+
+		public int SecondLanguage
+		{
+			get { return secondLanguage; }
+			set { secondLanguage = value; }
+		}
+
+		public int SocialScience
+		{
+			get { return socialScience; }
+			set { socialScience = value; }
+		}
+
+		public virtual int getTotalMarks()
+		{
+			this.totalMarks = maths + english + socialScience + secondLanguage;
+			return totalMarks;
+		}
+	}
+}
